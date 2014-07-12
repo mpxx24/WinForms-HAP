f71d4bc61c2a1260a891e0ce58e6bb0a0753c4af
mpxx24 <mpxx24@gmail.com>
2014-07-12 11:56:17 +0200

    1

---
+++ HAPreddit/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace HAPreddit
+{
+    public partial class Form1 : Form
+    {
+        public static IEnumerable<Rekordy> Znajdz() 
+        {
+            var strona = new HtmlWeb() { AutoDetectEncoding = false, OverrideEncoding = Encoding.GetEncoding("iso-8859-2") };
+            var dokument = strona.Load("http://www.reddit.com/r/csharp");
+            var linki = dokument.DocumentNode.Descendants("a").Where(x => x.Attributes.Contains("class") 
+                && x.Attributes["class"].Value.Contains("title may-blank")).ToList();
+            var listaRekordow = new List<Rekordy>();
+            foreach (var link in linki)
+            {
+                var rekord = new Rekordy();
+                rekord.nazwa = link.InnerText;
+                listaRekordow.Add(rekord);
+            }
+
+            return listaRekordow;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var listaRekordow = Znajdz();
+            var licznik = 0;
+            foreach (var rekordy in listaRekordow)
+            {
+                textBox1.Text += rekordy[licznik].nazwa; // + Environment.NewLine + Environment.NewLine;
+            }
+        }
+    }
+}
